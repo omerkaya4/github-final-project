3f45dc2e2376b390f431ac817dfb16d70bdc21d4
omerkaya4 <121867509+omerkaya4@users.noreply.github.com>
2023-07-20 09:22:41 +0300

    Update simple-interest.sh

--- simple-interest.sh
+++ simple-interest.sh
@@ -4,7 +4,7 @@
    # Do not use this in production. Sample purpose only.
    # Author: Upkar Lidder (IBM)
    # Additional Authors:
-   # <your GitHub username>
+   # omerkaya4
    # Input:
    # p, principal amount
    # t, time period in years
